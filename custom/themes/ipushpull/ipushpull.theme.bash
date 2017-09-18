2fc1705ee4634153a4389a7e5c2227600c05b45c
Tomino <dostal.it@gmail.com>
2017-09-18 15:38:10 +0100

    Update ipushpull.theme.bash

--- custom/themes/ipushpull/ipushpull.theme.bash
+++ custom/themes/ipushpull/ipushpull.theme.bash
@@ -7,7 +7,7 @@ SCM_THEME_PROMPT_DIRTY=" ${red}✗"
 SCM_THEME_PROMPT_CLEAN=" ${green}✓"
 
 prompt() {
-  PS1="$(scm_prompt_info)${reset_color}${cyan}[PROD][\u@\h][\w]${reset_color} $ "
+  PS1="$(scm_prompt_info)${reset_color}${cyan}[ENV][\u@\h][\w]${reset_color} $ "
 }
 
 LS_COLORS=$LS_COLORS:'di=1;36:ex=32:ln=33:or=31:' ; export LS_COLORS
