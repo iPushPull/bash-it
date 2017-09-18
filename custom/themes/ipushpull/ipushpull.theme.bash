c9e74f3960711962b6ce006e0ba11df707cdb7df
Tomino <dostal.it@gmail.com>
2017-09-18 15:09:56 +0100

    Create ipushpull.theme.bash
    Adding custom theme

---
+++ custom/themes/ipushpull/ipushpull.theme.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+SCM_GIT_SHOW_MINIMAL_INFO=true
+SCM_THEME_PROMPT_PREFIX="${cyan}(${green}"
+SCM_THEME_PROMPT_SUFFIX="${cyan})"
+SCM_THEME_PROMPT_DIRTY=" ${red}✗"
+SCM_THEME_PROMPT_CLEAN=" ${green}✓"
+
+prompt() {
+  PS1="$(scm_prompt_info)${reset_color}${cyan}[PROD][\u@\h][\w]${reset_color} $ "
+}
+
+LS_COLORS=$LS_COLORS:'di=1;36:ex=32:ln=33:or=31:' ; export LS_COLORS
+
+safe_append_prompt_command prompt
